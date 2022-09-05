1569698b6186059bbff9767be40bf58ae0c527a1
Vamsi-jammugani <112849928+Vamsi-jammugani@users.noreply.github.com>
2022-09-05 13:13:24 +0530

    Add files via upload

---
+++ vamsi sum.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+int main()
+{
+	int a,b,c,d,e,f,g;
+	a=7;
+	b=4;
+	c=a+b;
+	d=a-b;
+	e=a*b;
+	f=a/b;
+	g=a%b;
+    std:: cout <<"arthi operations addtion="<<c<<"\n";
+    std:: cout <<"arthi operations subtraction="<<d<<"\n";
+    std:: cout <<"arthi operations multplication="<<e<<"\n";
+    std:: cout <<"arthi operations division="<<f<<"\n";
+    std:: cout <<"arthi operations moudles="<<g<<"\n";
+	return 0;
+}
